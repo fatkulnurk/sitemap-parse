319b5c2f89ce0e0d38120c0f04e4465e356a3077
Fatkul Nur Koirudin <fatkulnurk@gmail.com>
2019-08-02 13:19:16 +0700

    first commit

--- lirik-lagu/liriklaguindonesia.net.js
+++ lirik-lagu/liriklaguindonesia.net.js
@@ -8,7 +8,7 @@ var date = Date().toString();
 sitemaps.parseSitemaps(end_point, function(url) { all_urls.push(url); }, function(err, sitemaps) {
     var i;
     const fs = require('fs');
-    var stream = fs.createWriteStream("result--" + date + ".txt");
+    var stream = fs.createWriteStream("result-" + end_point + "-" + date + ".txt");
     stream.once('open', function(fd) {
         for (i = 0; i < all_urls.length; i++) {
             stream.write(all_urls[i] + ',');
