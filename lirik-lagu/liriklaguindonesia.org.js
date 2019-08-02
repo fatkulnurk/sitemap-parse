f747c6b181440bb8fa707f22463394c894e3e505
Fatkul Nur Koirudin <fatkulnurk@gmail.com>
2019-08-02 13:47:14 +0700

    fix

--- lirik-lagu/liriklaguindonesia.org.js
+++ lirik-lagu/liriklaguindonesia.org.js
@@ -4,11 +4,13 @@ var end_point = 'https://liriklaguindonesia.org/sitemap_index.xml';
 var all_urls = [];
 
 var date = Date().toString();
+var name = "result-" + end_point + "-" + date + ".txt";
 
 sitemaps.parseSitemaps(end_point, function(url) { all_urls.push(url); }, function(err, sitemaps) {
     var i;
     const fs = require('fs');
-    var stream = fs.createWriteStream("result-" + end_point + "-" + date + ".txt");
+    fs.writeFile(name)
+    var stream = fs.createWriteStream(name);
     stream.once('open', function(fd) {
         for (i = 0; i < all_urls.length; i++) {
             stream.write(all_urls[i] + ',');
