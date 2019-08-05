6a318133f1689417be66b32a914ca3b607dcb7de
Fatkul Nur Koirudin <fatkulnurk@gmail.com>
2019-08-05 12:23:35 +0700

    update liriklagu.info

--- python-scrape/app-liriklaguinfo.py
+++ python-scrape/app-liriklaguinfo.py
@@ -16,6 +16,7 @@ for u in urls:
     sql = "SELECT * FROM lirik_lagu WHERE source = '" + url + "'"
     mycursor.execute(sql)
     myresult = mycursor.fetchall()
+    myresult = False
     
     if myresult:
         print('Gagal insert, data sudah ada.')
@@ -33,15 +34,25 @@ for u in urls:
                 clean_title = clean_title.replace(' | www.liriklagu.info', '')
                 clean_title = clean_title.replace(' | Lirik Lagu dot Info', '')
                 clean_title = clean_title.replace(' | Lirik Lagu dot Info www.liriklagu.info', '')
+                clean_title = clean_title.replace('Chord Gitar (Kord) ', '')
+                clean_title = clean_title.replace('Lirik Lagu dan Kord Gitar ', '')
+                clean_title = clean_title.replace('Lirik Lagu dan Chord Gitar (kord) ', '')
+                clean_title = clean_title.replace('Lirik ', '')
 
-                data_result_raw = soup.find('div', attrs= {'class': 'entry-content'})
+                data_result_raw = soup.find('pre')
                 if data_result_raw:
-                    clean_body = data_result_raw.prettify(formatter="html5")
-                    clean_body = clean_body.replace('&Acirc;', '')
+                    clean_body = data_result_raw.string
+                else:
+                    data_result_raw = soup.find('div', attrs= {'class': 'entry-content'})
+                    if data_result_raw:
+                        clean_body = data_result_raw.prettify(formatter="html5")
+                        clean_body = clean_body.replace('&Acirc;', '')
+                        clean_body = BeautifulSoup(clean_body, "lxml").text                        
+
+                if clean_body:
                     clean_slug = slugify(clean_title)
                     sql = "INSERT INTO lirik_lagu (title, body, source, author, post_date, chord, slug) VALUES (%s, %s, %s, %s, %s, %s, %s)"
                     val = (str(clean_title), str(clean_body), str(url), "", "", 'True', str(clean_slug))
                     mycursor.execute(sql, val)
                     mydb.commit()
                     print(mycursor.rowcount, "record inserted.")
-                    print('-----------------------------')
